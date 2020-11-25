1972f836625b5b9cc07e8cebae5ce5f16989c93f
sumancfc <sumanstha999@gmail.com>
2020-11-25 13:56:30 +0545

    Css changed for header

--- frontend/src/components/header/sub/MainMenu.js
+++ frontend/src/components/header/sub/MainMenu.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const MainMenu = ({ className }) => {
+const MainMenu = ({ className = "" }) => {
   return (
     <div className={`d-flex ${className}`}>
       <div className='header__main-menu'>
